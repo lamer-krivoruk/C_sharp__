4288706b79c0415f2ae9eb63c7fb7318eed264d3
lamer-krivoruk <kostik.telminov.80@mail.ru>
2024-05-29 23:43:03 +0300

    added task4

---
+++ begin/task4/Program.cs
@@ -0,0 +1,21 @@
+﻿int a = 5;
+int b = 78;
+int c = 3;
+int d = 10;
+int max = a;
+
+
+if(max < b)
+{
+    max = b;
+}
+if(max < c)
+{
+    max = c;
+}
+if(max < d)
+{
+    max = d;
+}
+Console.Write("Самая тяжелая гиря весит ");
+Console.WriteLine(max);
